4f705c0ccb085a8ee8771a1cfd480097c7832361
adeonita <adeonita.sousa@gmail.com>
2019-11-27 23:57:40 -0300

    Manutençao das funçoes register, generate_key e hash

--- hash.py
+++ hash.py
@@ -13,26 +13,21 @@ import hashlib
 
 class User:
 
-    def __init__(self, name, occupation, password):
-
-        self.name = name
-        self.occupation = occupation
-        self.password = password
-
-    def __eq__(self, other):
-
-        return self.name == other.name \
-            and self.occupation == other.occupation
-
-    def __str__(self):
-        return f'{self.name} {self.occupation}'
-
     def register():
        
         name = input("Insira seu nome: ")
         occupation = input("Insira sua ocupação: ") 
         password = getpass.getpass("Insira a senha: ")
         
+        return [name, password, occupation]
+
+    def generate_key():
+
+        valores = User.register()
+        name = valores[0]
+        password =  valores[1]
+        print(valores)
+
         value_name = 0
         value_password = 0
         for char in name:
@@ -41,52 +36,22 @@ class User:
             value_password = value_password + ord(char) 
         
         key = value_name + value_password
-
-
+        
         return key
 
+
     def hash(key):
-        indice = key % 23
-        print(indice)
-        
+       indice = key % 23
+       return indice
     
-    #def cadastrar():
-        
-    #    exist = os.path.isfile('cadastros.txt')
-    #    name = input("Insira seu nome: ")
-    #    occupation = input("Insira sua ocupação: ") 
-    #    password = getpass.getpass("Insira a senha: ")
-    #    key = name + password
-    #    print(key)
-
-    #    if(exist):
-    #        arquivo = open('cadastros.txt', 'r')
-    #        conteudo = arquivo.readlines()
-    #        conteudo.append("{}, {}\n".format(name, occupation))   # insira seu conteúdo
-    #        arquivo = open('cadastros.txt', 'w') # Abre novamente o arquivo (escrita)
-    #        arquivo.writelines(conteudo)    # escreva o conteúdo criado anteriormente nele.
-    #        arquivo.close()
-    #    else:
-    #        arquivo = open('cadastros.txt', 'w')
-    #        arquivo.write("{} , {}\n".format(name, occupation))
-    #        arquivo.close()
-
-        
        
        
-
-    def checar(name, occupation, password,):
-        if (u1 == u2):
-            print('same user')
-            print(f'{u1} == {u2}')
-        else:
-            print('different users')
 
 valor = 0
-while(valor <= 2 ):
-    usuario = User.register()
-    hash = User.hash(usuario)
-    
+while(valor <= 1 ):
+    usuario = User()
+    key = User.generate_key()
+    hash = User.hash(key)
     valor = valor + 1
 
 
